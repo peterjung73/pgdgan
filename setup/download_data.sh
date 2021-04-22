94c46d2cc9e1faece102b059ec725b5608d51103
Peter <peter.jung@tu-berlin.de>
2021-04-22 14:41:40 +0200

    get it working

--- setup/download_data.sh
+++ setup/download_data.sh
@@ -1,3 +1,4 @@
-wget https://www.cs.utexas.edu/~ashishb/csgm/celebAtest.zip
+# wget https://www.cs.utexas.edu/~ashishb/csgm/celebAtest.zip
+wget https://github.com/AshishBora/csgm/blob/master/celebAtest.zip?raw=true -O celebAtest.zip 
 unzip celebAtest.zip -d data/
 rm celebAtest.zip
